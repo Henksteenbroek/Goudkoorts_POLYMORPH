8d08c0f3e390336e8f7c094c9168ade4df4bd61c
sjors37 <sjorshekerman@live.nl>
2018-10-26 17:35:54 +0200

    thread

--- Goudkoorts/Controller/MainController.cs
+++ Goudkoorts/Controller/MainController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Goudkoorts.Controller
@@ -11,12 +12,25 @@ namespace Goudkoorts.Controller
     public class MainController
     {
         private Game game;
+        private Thread MyNewAwesomeThread;
+
         public MainController()
         {
             game = new Game();
             new MapCreator(game);
             new InputView();
             new OutputView(game);
+            MyNewAwesomeThread = new Thread(test);
+            MyNewAwesomeThread.Start();
+        }
+
+        private void test()
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.WriteLine(i++);
+            }
         }
     }
 }
