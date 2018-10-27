fa84f0488ef333cce5202ef0d3d3237718d23a28
Lpook <lpook@avans.nl>
2018-10-27 12:55:43 +0200

    .

--- Goudkoorts/View/InputView.cs
+++ Goudkoorts/View/InputView.cs
@@ -8,6 +8,59 @@ namespace Goudkoorts.View
 {
     public class InputView
     {
+        ConsoleKeyInfo key;
+        int returnValue;
 
+        public InputView()
+        {
+
+        }
+
+        public ConsoleKeyInfo validInputGiven()
+        {
+            bool validInputGiven = false;
+
+            Console.WriteLine("> gebruik pijltjestoetsen (s = stop, r = reset)");
+            while (!validInputGiven)
+            {
+                key = Console.ReadKey();
+                if (key.Key != ConsoleKey.UpArrow && key.Key != ConsoleKey.DownArrow && key.Key != ConsoleKey.LeftArrow && key.Key != ConsoleKey.RightArrow && key.Key != ConsoleKey.S && key.Key != ConsoleKey.R)
+                {
+                    Console.WriteLine(" is geen geldig teken, gebruik de pijltestoetsen of 's' of 'r'");
+                }
+                else
+                {
+                    validInputGiven = true;
+                }
+            }
+            return key;
+        }
+
+        public int readInput(ConsoleKeyInfo key)
+        {
+            this.key = key;
+            switch (key.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    returnValue = (int)Direction.Up;
+                    break;
+                case ConsoleKey.DownArrow:
+                    returnValue = (int)Direction.Down;
+                    break;
+                case ConsoleKey.LeftArrow:
+                    returnValue = (int)Direction.Left;
+                    break;
+                case ConsoleKey.RightArrow:
+                    returnValue = (int)Direction.Right;
+                    break;
+                case ConsoleKey.R:
+                    returnValue = -1;
+                    break;
+                case ConsoleKey.S:
+                    returnValue = -2;
+                    break;
+            }
+            return returnValue;
+        }
     }
 }
